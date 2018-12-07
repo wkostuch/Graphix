479558ff146c4b050104138677fa1acf0cc4b378
wkostuch <wkostuch@gmail.com>
2018-12-07 16:28:08 -0600

    Add workaround for file saving issue

--- src/backend/Graphix.java
+++ src/backend/Graphix.java
@@ -443,7 +443,12 @@ public class Graphix
 	public void writeGraph(String name) {
 		//Creates a file with the name in the graphs folder
 		String path = System.getProperty("user.dir");
-		File file = new File(path + "\\Graphix\\src\\graphs\\" + name + ".2dg");
+		//Bad workaround for file issues
+		if(!path.contains("Graphix")) {
+			path += "\\Graphix";
+		}
+			File file = new File(path + "\\src\\graphs\\" + name + ".2dg");
+		//File file = new File(path + "\\Graphix\\src\\graphs\\" + name + ".2dg");
 		BufferedWriter bw = null;
 		FileWriter fw = null;
 		
